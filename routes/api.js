b09fda7f4d4285a20212f2ae80b9041b9e4c0524
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com.>
2024-10-14 22:40:12 +0530

    feat: added caching via the express api-cache module to reduce server load when requesting the same endpoint.

--- routes/api.js
+++ routes/api.js
@@ -6,13 +6,17 @@ const Music = require("../models/music");
 const Product = require("../models/product");
 const Tag = require("../models/tags");
 const Variant = require("../models/variant");
+const apicache = require("apicache");
 
-// intializing a router object to handle 
+// initializing a router object to handle 
 // simila routes in the application
 const router = express.Router();
 
+// a cache middeware to cache api endpoints
+let cache = apicache.middleware
+
 // a simple get end-point to retrieve the required 'video' data from the database
-router.get('/homescreen', async function(req, res, next) {
+router.get('/homescreen', cache('5 minutes'), async function(req, res, next) {
     const video = await Video.findOne()
     .populate({ path: 'user', model: User}) 
     .populate({ path: 'music', model: Music}) 
